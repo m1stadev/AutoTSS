ac307994365b8e0d01bc7fc2cd653c6dfd89e6fd
marijuanARM <37033149+marijuanARM@users.noreply.github.com>
2021-01-30 01:30:12 -0600

    update on_guild_join message

--- cogs/events.py
+++ cogs/events.py
@@ -28,10 +28,18 @@ class Events(commands.Cog):
         else:
             channel = self.bot.get_channel(guild.text_channels[0].id)
 
+        user = await self.bot.fetch_user(728035061781495878)
+
         embed = discord.Embed(title="Hi, I'm AutoTSS!")
-        embed.add_field(name='What can I do?', value='I can automatically save SHSH blobs for all of your iOS devices!', inline=False)
+        embed.add_field(name='What do I do?', value='I can automatically save SHSH blobs for all of your iOS devices!', inline=False)
         embed.add_field(name='Prefix', value='My prefix is `b!`. To see what I can do, run `b!help`!', inline=False)
+        embed.add_field(name='Creator', value=user.mention, inline=False)
+        embed.add_field(name='Disclaimer', value='This should NOT be your only source for saving blobs. I am not at fault for any issues you may experience with AutoTSS.', inline=False)
+        embed.add_field(name='Notes', value='- There is a limit of 10 devices per user.\n- You must be in a server with AutoTSS, or your devices & blobs will be deleted. This **does not** have to be the same server that you added your devices to AutoTSS in.\n- Blobs are automatically saved every 30 minutes.', inline=False)
+        embed.add_field(name='Source Code', value="AutoTSS's source code can be found on [GitHub](https://github.com/marijuanARM/autotss).", inline=False)
+        embed.add_field(name='Support', value='For any questions about AutoTSS, join [my discord](https://discord.gg/fAngssA).', inline=False)
         embed.set_thumbnail(url=self.bot.user.avatar_url_as(static_format='png'))
+        embed.set_footer(text=ctx.author.name, icon_url=ctx.author.avatar_url_as(static_format='png'))
 
         await channel.send(embed=embed)
 

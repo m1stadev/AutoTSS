ed73ed706cfefb47ec6ad150dc1bdc307b8a769c
marijuanARM <37033149+marijuanARM@users.noreply.github.com>
2021-02-02 01:42:25 -0600

    remove unnecessary intent

--- bot.py
+++ bot.py
@@ -43,9 +43,7 @@ def get_prefix(client, message):
 if __name__ == '__main__':
     check_tsschecker()
 
-    intents = discord.Intents.default()
-    intents.members = True
-    client = commands.Bot(command_prefix=get_prefix, help_command=None, intents=intents)
+    client = commands.Bot(command_prefix=get_prefix, help_command=None)
 
     for cog in glob.glob('cogs/*.py'):
         client.load_extension(cog.replace('/', '.')[:-3])
@@ -54,6 +52,3 @@ if __name__ == '__main__':
         client.run(bot_token('token.txt'))
     except discord.LoginFailure:
         sys.exit("[ERROR] Token invalid, make sure your token is the only text in 'token.txt'. Exiting.")
-
-    except discord.errors.PrivilegedIntentsRequired:
-        sys.exit("[ERROR] Privileged Intents are not enabled. Go to 'https://discord.com/developers/applications', and enable the Server Members Intent. Exiting.")

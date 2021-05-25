9b8584df1ad5c7d566e124be3950d802f306ea7c
m1stadev <37033149+marijuanARM@users.noreply.github.com>
2021-05-25 12:23:39 -0500

    [cogs/misc.py] info cmd changes

--- cogs/misc.py
+++ cogs/misc.py
@@ -60,12 +60,10 @@ class Misc(commands.Cog):
 	@commands.command()
 	@commands.guild_only()
 	async def info(self, ctx):
-		user = await self.bot.fetch_user(728035061781495878)
-
 		embed = discord.Embed(title="Hi, I'm AutoTSS!")
 		embed.add_field(name='What do I do?', value='I can automatically save SHSH blobs for all of your iOS devices!', inline=False)
 		embed.add_field(name='Prefix', value='My prefix is `b!`. To see what I can do, run `b!help`!', inline=False)
-		embed.add_field(name='Creator', value=user.mention, inline=False)
+		embed.add_field(name='Creator', value=(await self.bot.fetch_user(728035061781495878)).mention, inline=False)
 		embed.add_field(name='Disclaimer', value='This should NOT be your only source for saving blobs. I am not at fault for any issues you may experience with AutoTSS.', inline=False)
 		embed.add_field(name='Notes', value='- There is a limit of 10 devices per user.\n- You must be in a server with AutoTSS, or your devices & blobs will be deleted. This **does not** have to be the same server that you added your devices to AutoTSS in.\n- Blobs are automatically saved every 30 minutes.', inline=False)
 		embed.add_field(name='Source Code', value="AutoTSS's source code can be found on [GitHub](https://github.com/m1stadev/autotss).", inline=False)

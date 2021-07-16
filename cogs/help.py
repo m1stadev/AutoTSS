4b2664373022a79be56a27a71c678541b8354537
m1stadev <37033149+m1stadev@users.noreply.github.com>
2021-07-16 03:15:45 -0500

    [help] Update embed formatting

--- cogs/help.py
+++ cogs/help.py
@@ -20,21 +20,23 @@ class Help(commands.Cog):
         embed.add_field(name='TSS Commands', value=f'`{prefix}help tss`', inline=False)
         embed.add_field(name='Miscellaneous Commands', value=f'`{prefix}help misc`', inline=False)
         embed.add_field(name="If this is your first time using AutoTSS, run this for more information", value=f'`{prefix}info`', inline=False)
+
         embed.set_footer(text=ctx.author.display_name, icon_url=ctx.author.avatar_url_as(static_format='png'))
         await ctx.send(embed=embed)
 
-    @help_command.command(name='device')
+    @help_command.command(name='devices', aliases=('device',))
     @commands.guild_only()
     async def device_commands(self, ctx: commands.Context) -> None:
         prefix = await self.utils.get_prefix(ctx.guild.id)
 
         embed = discord.Embed(title='Device Commands')
-        embed.set_footer(text=ctx.author.display_name, icon_url=ctx.author.avatar_url_as(static_format='png'))
         embed.add_field(name='Add a device', value=f'`{prefix}devices add`', inline=False)
         embed.add_field(name='Remove a device', value=f'`{prefix}devices remove`', inline=False)
         embed.add_field(name='List your devices', value=f'`{prefix}devices list`', inline=False)
         if await ctx.bot.is_owner(ctx.author):
             embed.add_field(name='Transfer devices to new user', value=f'`{prefix}devices transfer <old user> <new user>`', inline=False)
+
+        embed.set_footer(text=ctx.author.display_name, icon_url=ctx.author.avatar_url_as(static_format='png'))
         await ctx.send(embed=embed)
 
     @help_command.command(name='tss')
@@ -43,15 +45,14 @@ class Help(commands.Cog):
         prefix = await self.utils.get_prefix(ctx.guild.id)
 
         embed = discord.Embed(title='TSS Commands')
-        embed.set_footer(text=ctx.author.display_name, icon_url=ctx.author.avatar_url_as(static_format='png'))
         embed.add_field(name='Save blobs for all of your devices', value=f'`{prefix}tss save`', inline=False)
         embed.add_field(name='List all blobs saved for your devices', value=f'`{prefix}tss list`', inline=False)
         embed.add_field(name='Download all blobs saved for your devices', value=f'`{prefix}tss download`', inline=False)
-
         if await ctx.bot.is_owner(ctx.author):
             embed.add_field(name='Download all blobs saved for all devices', value=f'`{prefix}tss downloadall`', inline=False)
             embed.add_field(name='Save blobs for all devices', value=f'`{prefix}tss saveall`', inline=False)
 
+        embed.set_footer(text=ctx.author.display_name, icon_url=ctx.author.avatar_url_as(static_format='png'))
         await ctx.send(embed=embed)
 
     @help_command.command(name='misc')
@@ -60,11 +61,12 @@ class Help(commands.Cog):
         prefix = await self.utils.get_prefix(ctx.guild.id)
 
         embed = discord.Embed(title='Miscellaneous Commands')
-        embed.set_footer(text=ctx.author.display_name, icon_url=ctx.author.avatar_url_as(static_format='png'))
         embed.add_field(name='Change the command prefix for AutoTSS', value=f'`{prefix}prefix <prefix>`', inline=False)
         embed.add_field(name='Get the invite for this bot', value=f'`{prefix}invite`', inline=False)
         embed.add_field(name='See the latency of this bot', value=f'`{prefix}ping`', inline=False)
         embed.add_field(name='Information about AutoTSS', value=f'`{prefix}info`', inline=False)
+
+        embed.set_footer(text=ctx.author.display_name, icon_url=ctx.author.avatar_url_as(static_format='png'))
         await ctx.send(embed=embed)
 
     @help_command.command(name='admin')
@@ -75,6 +77,7 @@ class Help(commands.Cog):
 
         embed = discord.Embed(title='Admin Commands')
         embed.add_field(name='See module subcommands', value=f'`{prefix}module`', inline=False)
+
         embed.set_footer(text=ctx.author.display_name, icon_url=ctx.author.avatar_url_as(static_format='png'))
         await ctx.send(embed=embed)
 

17917d9daf9c48133cdfdb71f87df9188c8097d3
m1stadev <adamhamdi31@gmail.com>
2021-10-29 21:18:42 -0500

    [whitelist] Add checks back to commands (*whoops*)

--- cogs/whitelist.py
+++ cogs/whitelist.py
@@ -11,7 +11,7 @@ class WhitelistCog(commands.Cog, name='Whitelist'):
 
     @commands.group(name='whitelist', aliases=('w',), help='Whitelist management commands.', invoke_without_command=True)
     @commands.guild_only()
-    @commands.has_permissions(administrator=True)
+    @commands.max_concurrency(1, per=commands.BucketType.default)
     async def whitelist_group(self, ctx: commands.Context) -> None:
         help_aliases = (self.bot.help_command.command_attrs['name'], *self.bot.help_command.command_attrs['aliases'])
         if (ctx.subcommand_passed is None) or (ctx.subcommand_passed.lower() in help_aliases):
@@ -24,7 +24,8 @@ class WhitelistCog(commands.Cog, name='Whitelist'):
         await ctx.reply(embed=embed)
 
     @whitelist_group.command(name='set', help='Set the whitelist channel for AutoTSS commands.')
-    @commands.max_concurrency(1, per=commands.BucketType.guild)
+    @commands.guild_only()
+    @commands.max_concurrency(1, per=commands.BucketType.default)
     async def set_whitelist_channel(self, ctx: commands.Context, channel: discord.TextChannel) -> None:
         if channel.guild != ctx.guild:
             embed = discord.Embed(title='Error', description=f'{channel.mention} is not a valid channel.')
@@ -45,7 +46,8 @@ class WhitelistCog(commands.Cog, name='Whitelist'):
         await ctx.reply(embed=embed)
 
     @whitelist_group.command(name='toggle', help='Toggle the whitelist for AutoTSS commands on/off.')
-    @commands.max_concurrency(1, per=commands.BucketType.guild)
+    @commands.guild_only()
+    @commands.max_concurrency(1, per=commands.BucketType.default)
     async def toggle_whitelist(self, ctx: commands.Context) -> None:
         async with aiosqlite.connect('Data/autotss.db') as db:
             async with db.execute('SELECT * FROM whitelist WHERE guild = ?', (ctx.guild.id,)) as cursor:

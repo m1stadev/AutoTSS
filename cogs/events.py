862e5dea2b1bfe176748b130f83c6bad69860669
m1stadev <adamhamdi31@gmail.com>
2021-11-15 22:55:54 -0600

    [events] blob_saver: Move around some code

--- cogs/events.py
+++ cogs/events.py
@@ -33,14 +33,6 @@ class EventsCog(commands.Cog, name='Events'):
     async def blob_saver(self) -> None:
         await self.bot.wait_until_ready()
 
-        if self.utils.saving_blobs:
-            print(f"[AUTO] SHSH blob saver already running, continuing.")
-            await asyncio.sleep(300)
-            return
-
-        self.utils.saving_blobs = True
-        await self.bot.change_presence(activity=discord.Game(name='Ping me for help! | Currently saving SHSH blobs!'))
-
         async with self.bot.session.get('https://api.ipsw.me/v4/devices') as resp:
             devices = [d for d in await resp.json() if any(x in d['identifier'] for x in ('iPhone', 'AppleTV', 'iPod', 'iPad'))]
 
@@ -52,10 +44,17 @@ class EventsCog(commands.Cog, name='Events'):
             self._api
         except AttributeError:
             self._api = api
-            self.utils.saving_blobs = False
-            await self.utils.update_device_count()
             return
 
+        if self.utils.saving_blobs:
+            print(f"[AUTO] SHSH blob saver already running, continuing.")
+            await asyncio.sleep(300)
+            return
+
+        self.utils.saving_blobs = True
+        await self.bot.change_presence(activity=discord.Game(name='Ping me for help! | Currently saving SHSH blobs!'))
+
+        description = None
         for device in api.keys():
             if device not in self._api.keys(): # If new device is added to the API
                 print(f"[AUTO] New device has been detected: {device}.")
@@ -79,7 +78,6 @@ class EventsCog(commands.Cog, name='Events'):
                     start_time = await asyncio.to_thread(time.time)
                     data = await asyncio.gather(*[self.utils.sem_call(self.utils.save_user_blobs, user_data[0], json.loads(user_data[1])) for user_data in data])
                     finish_time = round(await asyncio.to_thread(time.time) - start_time)
-                    self.utils.saving_blobs = False
 
                     blobs_saved = sum(user['blobs_saved'] for user in data)
                     devices_saved = sum(user['devices_saved'] for user in data)
@@ -94,13 +92,14 @@ class EventsCog(commands.Cog, name='Events'):
                     else:
                         description = 'All SHSH blobs have already been saved.'
 
-                    print(f"[AUTO] {description}")
-                    await self.utils.update_device_count()
-                    await asyncio.sleep(300)
-                    return
+                    break
 
             else:
                 self._api[device] = api[device]
+
+            if description is not None:
+                print(f"[AUTO] {description}")
+                break
 
         self.utils.saving_blobs = False
         await self.utils.update_device_count()

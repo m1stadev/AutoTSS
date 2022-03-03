07e21fa530c8302e582ac8dd6635ff8c5d1fc7cc
m1stadev <adamhamdi31@gmail.com>
2022-03-02 22:03:14 -0600

    [errorhandler] Remove accidental comment

--- cogs/errorhandler.py
+++ cogs/errorhandler.py
@@ -70,7 +70,7 @@ class ErrorHandlerCog(commands.Cog, name='ErrorHandler'):
         elif isinstance(exc, commands.UserNotFound):
             embed.description = 'I could not find that user.'
 
-            # elif isinstance(exc, NoDevicesFound):
+        elif isinstance(exc, NoDevicesFound):
             embed.description = f"{'You have' if exc.user.id == ctx.author.id else f'{exc.user.mention} has'} no devices added."
 
         elif isinstance(exc, NoSHSHFound):

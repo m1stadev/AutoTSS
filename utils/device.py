bb3e3f2df416267a4090b59085cbf1cec3bfd572
m1stadev <adamhamdi31@gmail.com>
2022-03-15 19:01:31 -0500

    [device] Update - Still set attributes if args are none - Create init() function for initializing class - Add remove function for removing device from database - verify_ecid: More strict length verifying

--- utils/device.py
+++ utils/device.py
@@ -1,14 +1,17 @@
 from bot import DB_PATH
 from hashlib import sha1, sha384
+from typing import Optional
 from .errors import DeviceError
 from . import api
 
+import aiosqlite
 import asyncio
 import discord
+import ujson
 
 
 class Device:
-    def __init__(
+    async def init(  # A little hacky but it works ¯\_(ツ)_/¯
         self,
         user: discord.User,
         name: str = None,
@@ -17,45 +20,83 @@ class Device:
         boardconfig: str = None,
         generator: str = None,
         apnonce: str = None,
+        saved_blobs: list = None,
     ):
-        loop = asyncio.get_event_loop()
         self.user = user
+        self.name = self.verify_name(name)
+        self.identifier = await self.verify_identifier(identifier)
+        self.ecid = self.verify_ecid(ecid)
 
-        if name is not None:
-            self.name = self.verify_name(name, self.user.id)
-
-        if identifier is not None:
-            self.identifier = self.verify_identifier(identifier)
-
-        if ecid is not None:
-            self.ecid = self.verify_ecid(ecid)
-
-        if boardconfig is not None:
+        if boardconfig is None:
+            self.board = await self.get_board()
+        else:
             if getattr(self, 'identifier', None) is None:
-                raise DeviceError(
-                    'Device identifier must be set to fetch a board config.'
+                raise DeviceError('No device identifier provided.')
+
+            self.board = await self.verify_board(self.identifier, boardconfig)
+
+        self.generator = self.verify_generator(generator)
+        self.cpid = await self.get_cpid()
+        self.apnonce = self.verify_apnonce(apnonce)
+
+        return self
+
+    def to_dict(self) -> dict:
+        return {
+            'name': self.name,
+            'identifier': self.identifier,
+            'ecid': self.ecid,
+            'boardconfig': self.board,
+            'generator': self.generator,
+            'apnonce': self.apnonce,
+            'saved_blobs': list(),  # TODO: Handle saved blobs
+        }
+
+    async def remove(self) -> None:
+        async with aiosqlite.connect(DB_PATH) as db:
+            async with db.execute(
+                'SELECT devices from autotss WHERE user = ?',
+                (
+                    self.user.id,
+                ),  # TODO: Change so that it finds user via the ECID rather than checking with user ID
+            ) as cursor:
+                try:
+                    devices = ujson.loads((await cursor.fetchone())[0])
+                except TypeError:
+                    pass  # raise error
+
+            try:
+                device = next(d for d in devices if d['ecid'] == self.ecid)
+            except StopIteration:
+                raise DeviceError('Device not found in db???')
+
+            devices.remove(device)
+            if len(devices) == 0:
+                await db.execute('DELETE FROM autotss WHERE user = ?', (self.user.id,))
+            else:
+                await db.execute(
+                    'UPDATE autotss SET devices = ? WHERE user = ?',
+                    (ujson.dumps(devices), self.user.id),
                 )
 
-            self.boardconfig = self.verify_boardconfig(self.identifier, boardconfig)
-        else:
-            self.boardconfig = loop.run_until_complete(self.get_boardconfig())
+            await db.commit()
 
-        self.cpid = loop.run_until_complete(self.fetch_cpid())
-
-        if generator is not None:
-            self.generator = self.verify_generator(generator)
-
-        if apnonce is not None:
-            self.apnonce = self.verify_apnonce(self.identifier, apnonce)
+        del self
 
     # Data verification functions
-    async def verify_name(self, name: str, user: int) -> str:
+    def verify_name(self, name: str) -> Optional[str]:
+        if name is None:
+            return
+
         if len(name) > 20:  # Length check
             raise DeviceError("A device's name cannot be over 20 characters long.")
 
         return name
 
-    async def verify_identifier(self, identifier: str) -> str:
+    async def verify_identifier(self, identifier: str) -> Optional[str]:
+        if identifier is None:
+            return
+
         devices = await api.get_all_devices()
 
         if identifier.casefold() not in [
@@ -65,7 +106,10 @@ class Device:
 
         return identifier
 
-    async def verify_ecid(self, ecid: str) -> str:
+    def verify_ecid(self, ecid: str) -> Optional[str]:
+        if ecid is None:
+            return
+
         try:
             int(ecid, 16)  # Make sure the ECID provided is hexadecimal, not decimal
         except (ValueError, TypeError):
@@ -75,27 +119,33 @@ class Device:
         if ecid == 'abcdef0123456':  # This ECID is provided as an example in the modal
             raise DeviceError('Invalid ECID provided.')
 
-        if (
-            not 11 <= len(ecid) <= 13
+        if len(ecid) not in (
+            11,
+            13,
         ):  # All hex ECIDs without zero-padding are between 11-13 characters
             raise DeviceError('Invalid ECID provided.')
 
         return ecid
 
-    async def verify_boardconfig(self, identifier: str, boardconfig: str) -> str:
-        if boardconfig[-2:] != 'ap':
+    async def verify_board(self, identifier: str, board: str) -> Optional[str]:
+        if any(arg is None for arg in (identifier, board)):
+            return
+
+        if board[-2:] != 'ap':
             raise DeviceError('Invalid board config provided.')
 
-        device = await api.get_device(identifier)
+        device = await api.get_device_info(identifier)
         if not any(
-            b['boardconfig'].casefold() == boardconfig.casefold()
-            for b in device['boards']
-        ):  # If no boardconfigs for the given device identifier match the boardconfig, then return False
+            b['boardconfig'].casefold() == board.casefold() for b in device['boards']
+        ):  # If no boards for the given device identifier match the board, then return False
             raise DeviceError('Invalid board config provided.')
 
-        return boardconfig
+        return board
 
-    def verify_generator(self, generator: str) -> str:
+    def verify_generator(self, generator: str) -> Optional[str]:
+        if generator is None:
+            return
+
         if not generator.startswith('0x'):  # Generator must start wth '0x'
             raise DeviceError('Invalid nonce generator provided.')
 
@@ -111,13 +161,16 @@ class Device:
 
         return generator.lower()
 
-    def verify_apnonce(self, cpid: int, nonce: str) -> str:
+    def verify_apnonce(self, nonce: str) -> Optional[str]:
+        if any(arg is None for arg in (self.cpid, nonce)):
+            return
+
         try:
             int(nonce, 16)
         except ValueError or TypeError:
             raise DeviceError('Invalid ApNonce provided.')
 
-        if 0x8010 <= cpid < 0x8900:  # A10+ device ApNonces are 64 characters long
+        if 0x8010 <= self.cpid < 0x8900:  # A10+ device ApNonces are 64 characters long
             apnonce_len = 64
         else:  # A9 and below device ApNonces are 40 characters
             apnonce_len = 40
@@ -136,7 +189,10 @@ class Device:
 
         return gen_hash == self.apnonce
 
-    async def verify_apnonce_pair(self) -> bool:
+    async def verify_apnonce_pair(self) -> Optional[bool]:
+        if any(arg is None for arg in (self.apnonce, self.generator)):
+            return
+
         return await asyncio.to_thread(self._verify_apnonce_pair)
 
     @property
@@ -146,7 +202,7 @@ class Device:
 
         return ('*' * len(self.ecid))[:-4] + self.ecid[-4:]
 
-    async def get_boardconfig(self) -> str:
+    async def get_board(self) -> str:
         if getattr(self, 'identifier', None) is None:
             raise DeviceError('No identifier for this device is set.')
 
@@ -168,9 +224,9 @@ class Device:
         return valid_boards[0].lower()
 
     async def get_cpid(self) -> str:
-        for attr in ('boardconfig', 'identifier'):
+        for attr in ('board', 'identifier'):
             if getattr(self, attr, None) is None:
-                raise DeviceError(f'No {attr} for this device is set.')
+                return
 
         device = await api.get_device_info(self.identifier)
 
@@ -178,7 +234,7 @@ class Device:
             return next(
                 b['cpid']
                 for b in device['boards']
-                if b['boardconfig'].casefold() == self.boardconfig.casefold()
+                if b['boardconfig'].casefold() == self.board.casefold()
             )
         except StopIteration:
             raise DeviceError('Failed to retrieve the CPID for this device.')
